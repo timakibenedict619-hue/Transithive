f35bcddcb2cd9648df91380f5802c758d4ab4c0d
timakibenedict619-hue <timakibenedict619@gmail.com>
2026-07-12 20:28:16 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,28 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1rb8p4KsKaxQK_QVtjWbFE5CI",
+  authDomain: "swifttrans-logistics.firebaseapp.com",
+  projectId: "swifttrans-logistics",
+  storageBucket: "swifttrans-logistics.firebasestorage.app",
+  messagingSenderId: "336087148680",
+  appId: "1:336087148680:web:d3fb690dd18d2337ca973d",
+  measurementId: "G-0W702ZGW7M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
