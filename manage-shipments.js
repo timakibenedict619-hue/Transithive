72d1567bbf08c8a226a9d4a59cc388a0694e0c38
timakibenedict619-hue <timakibenedict619@gmail.com>
2026-07-12 19:59:29 +0100

    Delete manage-shipments.js

--- manage-shipments.js
+++
@@ -1,5 +0,0 @@
-import {db} from './firebase.js';
-import {collection,getDocs} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
-const body=document.getElementById('rows');
-const snap=await getDocs(collection(db,'shipments'));
-snap.forEach(d=>{const s=d.data();body.innerHTML+=`<tr><td>${s.trackingId}</td><td>${s.status}</td><td><a href="shipment-details.html?id=${d.id}">View</a></td></tr>`});
