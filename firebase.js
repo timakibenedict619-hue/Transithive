447e1602c005273085c0a3b9276de11fe9d3e0cf
timakibenedict619-hue <timakibenedict619@gmail.com>
2026-07-13 05:16:11 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,29 +1,30 @@
-// Firebase SDK
+// firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your Firebase Configuration
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1rb8p4KsKaxQK_QVtjWbFE5IStV1F5CI",
-  authDomain: "swifttrans-logistics.firebaseapp.com",
-  projectId: "swifttrans-logistics",
-  storageBucket: "swifttrans-logistics.firebasestorage.app",
-  messagingSenderId: "336087148680",
-  appId: "1:336087148680:web:d3fb690dd18d2337ca973d",
-  measurementId: "G-0W702ZGW7M"
+    apiKey: "AIzaSyC1rb8p4KsKaxQK_QVtjWbFE5IStV1F5CI",
+    authDomain: "swifttrans-logistics.firebaseapp.com",
+    projectId: "swifttrans-logistics",
+    storageBucket: "swifttrans-logistics.firebasestorage.app",
+    messagingSenderId: "336087148680",
+    appId: "1:336087148680:web:d3fb690dd18d2337ca973d",
+    measurementId: "G-0W702ZGW7M"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Authentication
+const auth = getAuth(app);
 
-export default app;
+// Firestore Database
+const db = getFirestore(app);
+
+// Export Firebase services
+export { app, auth, db };
